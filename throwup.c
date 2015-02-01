b198f903038ea2f2c420cea600ba2101107fa343
epg5 <epg5@njit.edu>
2015-01-31 23:12:20 -0500

    gibs baby

--- throwup.c
+++ throwup.c
@@ -6,21 +6,21 @@
 //this makes you throw up
 void ThrowUpNow(edict_t*self)
 {
-	vec3_t foward, right;
+	vec3_t forward, right;
 	vec3_t mouth_pos, spew_vector;
 	float rnum;
 	int i;
 
 	//setting spew vector based on players view angles
-	AngleVectors(self->client->v_angle, foward, right, NULL);
+	AngleVectors(self->client->v_angle, forward, right, NULL);
 
 	//make the vomit originate from the players mouth
-	VectorScale(foward, 24, mouth_pos);
+	VectorScale(forward, 24, mouth_pos);
 	VectorAdd(mouth_pos, self->s.origin, mouth_pos);
 	mouth_pos[2] += self->viewheight;
 
 	//make the vomit come foward from the players mouth
-	VectorScale(foward, 24, spew_vector);
+	VectorScale(forward, 24, spew_vector);
 
 	//blood opied from the spawn damage function
 	gi.WriteByte (svc_temp_entity);
@@ -46,4 +46,17 @@ void ThrowUpNow(edict_t*self)
 		gi.sound(self, CHAN_BODY, SexedSoundIndex(self, "gurp2"), 1, ATTN_NORM, 0);
 	*/
 	gi.sound(self, CHAN_VOICE, gi.soundindex("misc/udeath.wav"), 1, ATTN_NORM, 0);
+
+	// throw up gibs.
+	for (i = 0; i<3; i++) {
+		ThrowVomit (self, mouth_pos, forward, right, self->velocity);
+	}
+
+	// every now and again, cough up MEGA vomit
+	if (random() < 0.1)
+	{
+		for (i = 0; i<10; i++) {
+			ThrowVomit (self, mouth_pos, forward, right, self->velocity);
+		}
+	}
 }
--- throwup.h
+++ throwup.h
@@ -2,3 +2,6 @@
 
 //Main Function
 void ThrowUpNow(edict_t*self);
+
+//gibs
+void ThrowVomit (edict_t *ent, vec3_t mouth_pos, vec3_t forward, vec3_t right, vec3_t player_vel);
